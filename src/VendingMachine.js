4c724448cc687bfe61bcd34da56ae554aef19c5e
cdsalty <soltiscd@gmail.com>
2020-01-02 17:21:54 -0500

    updated content, reverted back

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Message from "./Message";
+
 import "./VendingMachine.css";
 import vendingMachineImg from "./VendingMachine.png";
 
@@ -8,23 +8,15 @@ class VendingMachine extends Component {
   render() {
     return (
       <div
-        className='VendingMachine'
+        className="VendingMachine"
         style={{ backgroundImage: `url(${vendingMachineImg})` }}
       >
-        <Message>
-          <h1>hello i am a vending machine. what would you like to eat?</h1>
-        </Message>
-        <Message>
-          <h1>
-            <Link to='/soda'>Soda</Link>
-          </h1>
-          <h1>
-            <Link to='/chips'>Chips</Link>
-          </h1>
-          <h1>
-            <Link to='/sardines'>Sardines</Link>
-          </h1>
-        </Message>
+        <h1>hello i am a vending machine. what would you like to eat?</h1>
+        {/* first, created the routes in App.js and matching them up here accordingly */}
+        {/* Link always comes with ' to ' */}
+        <Link exact to="/soda">
+          Soda
+        </Link>
       </div>
     );
   }
